1267d8a6dc53878f9a7514e1c46285ca92279230
Matthew Green <LordFusion@users.noreply.github.com>
2020-02-29 13:02:25 -0600

    TeleportHere initial implementation

--- src/main/java/io/github/lordfusion/fusiontp/commands/TeleportHere.java
+++ src/main/java/io/github/lordfusion/fusiontp/commands/TeleportHere.java
@@ -1,11 +1,20 @@
 package io.github.lordfusion.fusiontp.commands;
 
+import io.github.lordfusion.fusiontp.DataManager;
+import io.github.lordfusion.fusiontp.FusionTP;
+import io.github.lordfusion.fusiontp.utilities.Teleporter;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class TeleportHere implements CommandExecutor
 {
+    private static TextComponent ERROR_CONSOLE, ERROR_INVALID_TARGET, ERROR_FAILURE,
+            SUCCESS_TARGET, SUCCESS_COMMANDSENDER;
+    
     /**
      * Executes the given command, returning its success.
      * <br>
@@ -21,6 +30,31 @@ public class TeleportHere implements CommandExecutor
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
     {
-        return false;
+        if (args.length != 1)
+            return false;
+        if (!(sender instanceof Player)) {
+            FusionTP.sendUserMessage(sender, ERROR_CONSOLE);
+            return true;
+        }
+    
+        OfflinePlayer source = ((Player)sender).getPlayer();
+        OfflinePlayer target = DataManager.findPlayer(args[0]);
+        
+        if (target == null) {
+            TextComponent msg = (TextComponent)ERROR_INVALID_TARGET.duplicate();
+            msg.addExtra('\"' + args[0] + '\"');
+            FusionTP.sendUserMessage(sender, msg);
+            FusionTP.sendConsoleInfo("Failed to teleport; invalid target.");
+            return true;
+        }
+    
+        if (Teleporter.teleport(source, target)) {
+            FusionTP.sendUserMessage(sender, SUCCESS_COMMANDSENDER);
+            if (target.isOnline())
+                FusionTP.sendUserMessage(sender, SUCCESS_TARGET);
+        } else {
+            FusionTP.sendUserMessage(sender, ERROR_FAILURE);
+        }
+        return true;
     }
 }
