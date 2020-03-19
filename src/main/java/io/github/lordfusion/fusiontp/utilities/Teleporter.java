5343290087143429f2b7ecae95950b6395efddbd
Matthew Green <mgreen9800@gmail.com>
2020-03-19 16:06:16 -0500

    Add teleport methods

--- src/main/java/io/github/lordfusion/fusiontp/utilities/Teleporter.java
+++ src/main/java/io/github/lordfusion/fusiontp/utilities/Teleporter.java
@@ -1,5 +1,19 @@
 package io.github.lordfusion.fusiontp.utilities;
 
+import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
+
 public class Teleporter
 {
+    public static boolean teleport(OfflinePlayer source, OfflinePlayer target)
+    {
+        
+        return false;
+    }
+    
+    public static boolean teleport(OfflinePlayer source, Location target)
+    {
+        
+        return false;
+    }
 }
